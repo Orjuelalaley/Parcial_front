9f1a392a70d8f9c9d03ac7682b6536d54060c5fe
Sebastian Orjuela <sebasorjuela@gmail.com>
2023-05-25 20:00:52 -0500

    El codigo ya realiza todo lo solicitado en front end

--- src/app/Service/service.service.ts
+++ src/app/Service/service.service.ts
@@ -8,13 +8,13 @@ import { Libro } from '../Modelo/Libro';
 export class ServiceService {
 
   constructor(private http:HttpClient) { }
-  Url='http://localhost:8080/api/v1/libro/all';
+  Url='http://localhost:8080/api/v1/libro';
 
   getLibros(){
-    return this.http.get<Libro[]>(this.Url);
+    return this.http.get<Libro[]>(this.Url+"/all");
   }
 
   PostLibro(libro:Libro){
-    return this.http.post<Libro>(this.Url,libro);
+    return this.http.post<Libro>(this.Url+"/add",libro);
   }
 }
